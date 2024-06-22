729f9829f62084860ed80ace2f137bc724c2508d
krishnakumar34 <58246610+krishnakumar34@users.noreply.github.com>
2024-06-23 01:11:19 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -97,6 +97,6 @@ module.exports = async (req, res) => {
 
 async function get(mod, parsed) {
     return await new Promise((resolve, reject) => {
-        await mod.get(parsed, resolve).once('error', reject);
+        mod.get(parsed, resolve).once('error', reject);
     });
 }
